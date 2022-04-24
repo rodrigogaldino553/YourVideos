d03bfb200cfa5e5521f7482d257f0a8fcb679063
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-04-24 15:48:23 -0300

    fix permit params for devise

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "sqlite3", "~> 1.4"
+  gem "pry"  
+  gem "pry-doc"
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
+    # binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    # update edit of avatar doesnt works
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    # binding.pry
   end
 end
